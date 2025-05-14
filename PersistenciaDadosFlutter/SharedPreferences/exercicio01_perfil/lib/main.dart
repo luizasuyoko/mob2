7d78af4405ce091fcdd53bd660f200b4281e0e5b
luizasuyoko <luizasuemiyokomizo@outlook.com>
2025-05-14 14:36:17 -0300

    14.05.2025

---
+++ PersistenciaDadosFlutter/SharedPreferences/exercicio01_perfil/lib/main.dart
@@ -0,0 +1,118 @@
+import 'dart:ffi';
+import '../../../../exercicio01_perfil/.dart_tool/package_config_subset';
+import '../../../../exercicio01_perfil/.gitignore';
+import '../../../../exercicio01_perfil/android/app/src/main/AndroidManifest.xml';
+
+void main(){ //método principal para rodae a aplicacao
+  runApp(MaterialApp( //base d todos os widgets(Elementos visuais)
+    home: TelaPerfil(), //tela inicial
+    //routes: 
+
+    //} //rotas de navegação
+    //theme: , //thema do aplicativo
+    //darktheme: , //tema alternativo
+    debugShowCheckModeBanner: false, //remove o banner vermelho
+  ));
+}
+
+class TelaPerfil extends StatefulWidget{ //tela dinamica
+  @override
+  State<TelaPerfil> createState() => _TelaPerfilState(); //chama a mudança
+}
+
+class _TelaPerfilState extends State<TelaPerfil>{//realiza a construção da tela
+  TextEditingController _nomeController = TextEditingController();
+  TextEditingController _idadeController = TextEditingController();
+
+  String? _nome;
+  double? _idade;
+
+  Map<String,Color> coresDisponiveis ={
+    "Azul": Colors.blue,
+    "Verde": Colors.green,
+    "Vermelho": Colors.red,
+    "Amarelo": Colors.yellow,
+    "Cinza": Colors.grey,
+    "Preto": Colors.black,
+    "Branco": Colors.white,
+
+    Color _corFundo = Colors.white;
+
+    //metodos
+    @override
+    Void initState(){
+      super.initState();
+      _carregarPreferencias()
+    }
+
+    _carregarPreferencias() async{
+      SharedPreferences _prefs =await SharedPreferences.getInstance();
+      setState(() {
+        _nome = _prefs.getString("nome");
+        _idade = _prefs.getDouble("idade");
+        _cor = _prefs.getString("cor");
+        if(_cor != null){
+          _corFundo = coresDisponiveis[_cor!]!;
+        }
+      });
+    }
+
+    _salvarPreferencias( async{
+      SharedPreferences_prefs = awaitSharedPreferences.getInstance()
+      await _prefs.setString("nome", _nomeController.text.trim());
+      await _prefs.setDouble("idade", _idadeController.text.trim());
+      await _prefs.setString("cor", _cor ?? "Branco");
+      setState(() {})
+    })
+  };
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: _corFundo,
+      appBar: AppBar(title: Text("Meu Perfil Persistente"), backgroundColor: Colors.purple,)
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: ListView(
+          children: [
+            TextField(
+              controller: _nomeController,
+              decoration: InputDecoration(labelText: "Idade"),
+              keyboardType: TextInputType.numberWithOptions(), 
+            ),
+            SizedBox(height:16),
+            DropdownButtonFormField(
+              value: _cor,
+              decoration: InputDecoration(labelText: "Core Favorita"),
+              items: coresDisponiveis.keys.map(
+                (cor){
+                  return DropdownMenuItem(
+                    value: _cor,
+                    child: Text(cor));
+                }
+              ).toList(),
+              onChanged: (valor){
+                setState(() {
+                  _cor = valor;
+                });
+              }SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: _salvarPreferencias, child: Text("Salvar dados")),
+              SizedBox: (height: 16),
+              Divider(),
+              SizedBox: (height: 16),
+              Text("Dados Salvos:"),
+              if(_nome != null)
+                Text("Nome: $_nome"),
+              if(_idade != null)
+                Text("Idade: $_idade"),
+              if(_cor != null)
+                Text("Cor: $_cor"),
+              
+            )
+          ]
+        )
+      )
+    )
+  }
+}
