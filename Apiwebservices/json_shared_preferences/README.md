a1819c85e56e903854bad3a8573402db67e0fca8
luizasuyoko <luizasuemiyokomizo@outlook.com>
2025-08-12 11:11:27 -0300

    12.08.2025

--- Apiwebservices/json_shared_preferences/README.md
+++ Apiwebservices/json_shared_preferences/README.md
@@ -1,3 +1,21 @@
 # json_shared_preferences
 
 A new Flutter project.
+
+Shared Preferences (Armazenamento Interno do Aplicativo)
+
+Armazenamento Chave -> Valor
+            "config" -> "Texto" texto em formato Json
+
+O que é um Txto em formato Json -> 
+[
+    config:{
+        "NomedoUsuario": "nome do usuário",
+        "IdadedoUsuario": 25,
+        "TemaEscuro": true,
+    }
+]
+
+dary -> Linguagem de Programação do Flutter não lê JSON
+    -> converter => (json.decode => converte texto json em Map:dart)
+                 => (json.decode => convert Map:Dart em Text:Json)
--- Apiwebservices/json_shared_preferences/lib/main.dart
+++ Apiwebservices/json_shared_preferences/lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:convert'; //pacote dart (ja vem instalado no projeto)
 import 'package:flutter/material.dart';
+import 'package:json_shared_preferences/tela_inicial.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main(){
@@ -8,7 +10,6 @@ void main(){
 }
 
 class ConfigPage extends StatefulWidget {
-  const ConfigPage({super.key});
 
   @override
   State<StatefulWidget> createState() {
@@ -19,6 +20,7 @@ class ConfigPage extends StatefulWidget {
 class _ConfigPageState extends State<ConfigPage> {
   //atributos
   bool temaEscuro = false;
+  String nomeUsuario = ""; //texto vazio
 
   //método que roda antes de carregar a pagina
   @override
@@ -27,12 +29,29 @@ class _ConfigPageState extends State<ConfigPage> {
     carregarPreferencias();
   }
 
+  //método para carregar as informações do sharedpreferences
   void carregarPreferencias() async {
-    final prefs = await SharedPreferences.getInstance();
+    final prefs = await SharedPreferences.getInstance(); //conexão com sharedPreferences
+    String? jsonString = prefs.getString("config"); //estou recebendo os valores referentes a chave config do sharedpreferences
+    if (jsonString != null){
+      Map<String, dynamic> config = json.decode(jsonString); 
+      setState(() { //método para mudança de estudo
+        temaEscuro = config["temaEscuro"] ?? false; //?? operador para elemento null => atribui um valor caso o elemento seja nulo
+        nomeUsuario = config["nome"] ?? "";
+      });
+    }
   }
 
+  //contrução da tela
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return MaterialApp(
+      title: "App de configurações",
+      theme: temaEscuro ? ThemeData.dark() : ThemeData.light(), //operador ternário
+      home: TelaInicial(
+        temaEscuro: temaEscuro,
+        nomeUsuario: nomeUsuario,
+        ),
+    );
   }
 }
